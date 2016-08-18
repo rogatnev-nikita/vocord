39b9cc0feb1bbbc15b8ddeb80ebd4f9dab267afe
Rogatnev Nikita <mail@rogatnev.ru>
2016-08-18 21:07:58 +0300

    header init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+
+	// Home page image animations
+	$(".catalog__item img").each(function (e) {
+		var src = $(this).attr("src");
+
+		$(this).hover(function () {
+			$(this).attr("src", src.replace("--static.png", ".gif"));
+		}, function () {
+			$(this).attr("src", src);
+		});
+	});
+
+	// Add body class when mobile menu collapsed
+
+	$(".navbar-toggle").click(function () {
+		$(".header__logo").toggleClass("hidden");
+		$(".navbar-toggle").toggleClass("navbar-toggle--active");
+		$("body").toggleClass("body--invert");
+	});
+
+});
