fd21d804d5dfe27b074501423badba005c90ad55
Rogatnev Nikita <mail@rogatnev.ru>
2016-08-19 20:55:30 +0300

    faces intro & feature blocks init

--- js/script.js
+++ js/script.js
@@ -10,13 +10,23 @@ $(document).ready(function () {
 		});
 	});
 
-	// Add body class when mobile menu collapsed
+	// Feature block image animations
+	$(".feature__image img").each(function (e) {
+		var src = $(this).attr("src");
+		$(this).hover(function () {
+			$(this).attr("src", src.replace(".jpg", "--tooltip.jpg"));
+		}, function () {
+			$(this).attr("src", src);
+		});
+	});
+
+	// Hide logo when mobile menu collapse
 	$(".navbar-toggle").click(function () {
 		$(".header__logo").toggleClass("hidden");
 		$(".navbar-toggle").toggleClass("navbar-toggle--active");
 	});
 
-	// Header search field
+	// Header search
 	$(".header__sub-menu li:nth-child(3) a, .header__search-close-icon").click(function () {
 		$(".header__search-input, .header__main-menu, .header__sub-menu, .header__search").toggleClass("hidden");
 		$(".header__search-input").focus();
