49db5abd0b9e1816bedd00f837123b8352a6a500
Rogatnev Nikita <mail@rogatnev.ru>
2016-08-24 18:15:04 +0300

    Technology slider navigation arrows

--- js/script.js
+++ js/script.js
@@ -33,4 +33,12 @@ $(document).ready(function () {
 		event.preventDefault();
 	});
 
+	// Technology slider navigation arrows
+	$('.technology__slider-arrow--right').click(function () {
+		$('.technology__slider-tablist li').filter('.active').next('li').find('a[data-toggle="tab"]').tab('show');
+	});
+	$('.technology__slider-arrow--left').click(function () {
+		$('.technology__slider-tablist li').filter('.active').prev('li').find('a[data-toggle="tab"]').tab('show');
+	});
+
 });
